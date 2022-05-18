71e7300fcaf1489b25f88ca590b38fadae793db9
Matt Keenan <mkeenan20@gmail.com>
2022-05-18 08:11:31 -0400

    work

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 // Define basic Express configs
+const { application } = require('express');
 const express = require('express');
+const request = require('http').request()
 const app = express();
 const port = 8008;
 const path = require('path');
@@ -8,6 +10,11 @@ const path = require('path');
 // Define some basic endpoints
 const api = '/api/v1/';
 const health = `health/`;
+const carbonAPI = 'https://www.carboninterface.com/api/v1';
+
+
+// DELETE BEFORE COMMIT
+const token = '';
 
 // Serve static assets
 app.use('', express.static(path.join(__dirname, 'public')));
@@ -19,6 +26,24 @@ app.get(api, (req, res) => {
 
 app.get(api + health, (req, res) => {
     res.send('OK');
+});
+
+app.get(api+'test', (req, res) => {
+
+    const request = new Request(`${carbonAPI}/auth`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        method: 'GET',
+    })
+    fetch(request)
+    .then(val => {
+        console.log('Auth Response', val.json());
+    })
+    .catch(err => {
+        console.err(err);
+    })
+
 });
 
 
