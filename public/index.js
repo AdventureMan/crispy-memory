a19ef31e4dd8d58e5d78dfecb66dedb4f0ba82b8
Matt Keenan <mkeenan20@gmail.com>
2022-05-22 20:35:23 -0400

    Add some UI and connect frontend to middle api

--- public/index.js
+++ public/index.js
@@ -1,5 +1,11 @@
 
+const API = 'api/v1/calculate'
 
+const planeBlock = document.getElementById('plane-inputs');
+const vehicleBlock = document.getElementById('vehicle-inputs');
+const shippingBlock = document.getElementById('shipping-inputs');
+
+let currentForm = null;
 
 
 window.onload = (e) => {
@@ -12,5 +18,62 @@ window.onload = (e) => {
 
 function setTransport(ev) {
     console.log('setTransport', ev);
+    currentForm = document.getElementById(`form-${ev}`);
 
+    planeBlock.hidden = true;
+    vehicleBlock.hidden = true;
+    shippingBlock.hidden = true;
+
+
+    document.getElementById(`${ev}-inputs`).hidden = false;
+}
+
+
+function getCalc(formType) {
+    let type = formType;
+    const form = currentForm.elements;
+    let data = {};
+
+    switch (formType) {
+        case 'plane':
+            data = {
+                departure: form.inputDeparture.value,
+                destination: form.inputDestination.value
+            };
+            break;
+        case 'vehicle':
+            data = {
+                make: form.inputMake.value,
+                model: form.inputModel.value,
+                distance: form.inputVehDist.value
+            };
+            break;
+        case 'shipping':
+            data = {
+                weight: form.inputWeight.value,
+                distance: form.inputShipDist.value,
+                method: form.inputShipMethod.value
+            };
+            break;
+    }
+
+    const request = {
+        type,
+        data
+    };
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    const apiInit = {
+        method: 'POST',
+        body: JSON.stringify(request),
+        headers
+    };
+
+    fetch(API, apiInit)
+        .then((response) => {
+            console.log('Something went right!', response);
+
+        }, (err) => {
+            console.error('Something went wrong', err);
+        });
 }
--- server.js
+++ server.js
@@ -2,50 +2,81 @@
 // Define basic Express configs
 const { application } = require('express');
 const express = require('express');
-const request = require('http').request()
 const app = express();
-const port = 8008;
+const port = 8080;
 const path = require('path');
 
 // Define some basic endpoints
-const api = '/api/v1/';
-const health = `health/`;
+const api = '/api/v1';
+const health = `/health`;
 const carbonAPI = 'https://www.carboninterface.com/api/v1';
+const POST_CALC = '/calculate'
+
+
+
+// Define some classes for data because I'm feeling fancy
+class FlightRequest {
+    constructor(departure, destination) {
+        this.departure = departure;
+        this.destination = destination;
+    }
+}
+
+class VehicleRequest {
+    constructor(make, model, distance) {
+        this.make = make;
+        this.model = model;
+        this.distance = distance;
+    }
+}
+
+class ShippingRequest {
+    constructor(weight, distance, method) {
+        this.weight = weight;
+        this.distance = distance;
+        this.method = method;
+    }
+}
 
 
 // DELETE BEFORE COMMIT
 const token = '';
 
-// Serve static assets
+// Serve static assets and include JSON body parsing middleware
 app.use('', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-
+// Healthcheck endpoints
 app.get(api, (req, res) => {
     res.send('Hello there!');
 });
-
-app.get(api + health, (req, res) => {
+app.get(`${api}${health}`, (req, res) => {
     res.send('OK');
 });
 
-app.get(api+'test', (req, res) => {
-    const request = new Request(`${carbonAPI}/auth`, {
 
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
-        method: 'GET',
-    })
-    fetch(request)
-    .then(val => {
-        console.log('Auth Response', val.json());
-    })
-    .catch(err => {
-        console.err(err);
-    })
+// API to handle form input
+app.post(`${api}${POST_CALC}`, (request, response) => {
 
-});
+    if (!request) {
+        response.status(400).send(JSON.stringify(new Error('malformed request')));
+        return;
+    }
 
+    let retObj = {};
+    if (request.body.type === 'plane') {
+        retObj = new FlightRequest(request.body.data.departure, request.body.data.destination);
+    } else if (request.body.type === 'vehicle') {
+        retObj = new VehicleRequest(request.body.data.make, request.body.data.model, request.body.data.distance);
+    } else {
+        retObj = new ShippingRequest(request.body.data.weight, request.body.data.distance, request.body.data.method);
+    }
+
+    response.json(retObj);
+    retObj = null;
+    
+
+})
 
 
 
