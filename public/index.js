3acc4d9b0ce9e141ef31c75e665d72974e82da49
Matt Keenan <mkeenan20@gmail.com>
2022-05-26 20:49:07 -0400

    add prettier because

--- public/index.js
+++ public/index.js
@@ -1,6 +1,5 @@
-
 // Set our backend API path
-const API = 'api/v1/calculate'
+const API = 'api/v1/calculate';
 const API_VHIC_MODELS = 'api/v1/vehicles';
 
 // Handy references to our form wrappers on the DOM
@@ -22,17 +21,15 @@ const shippingSubmit = document.getElementById('shippingSubmit');
 const makesDropDown = document.getElementById('inputMake');
 const modelDropdown = document.getElementById('inputModel');
 
-
 // Handy reference to the form in use
 let currentForm = null;
 
-
 window.onload = () => {
-    console.log('Window loaded');
-    fetchVhicMakes();
-}
+  console.log('Window loaded');
+  fetchVhicMakes();
+};
 
-makesDropDown.addEventListener('change',  (e) => fetchVhicModel(e));
+makesDropDown.addEventListener('change', (e) => fetchVhicModel(e));
 planeInput.addEventListener('click', () => setTransport('plane'));
 vehicleInput.addEventListener('click', () => setTransport('vehicle'));
 shippingInput.addEventListener('click', () => setTransport('shipping'));
@@ -41,143 +38,132 @@ planeSubmit.addEventListener('click', () => getCalc(currentForm));
 vehicleSubmit.addEventListener('click', () => getCalc(currentForm));
 shippingSubmit.addEventListener('click', () => getCalc(currentForm));
 
-
-
 function fetchVhicMakes() {
-
-    // Call and respond to the API
-    fetch(API_VHIC_MODELS)
-        .then((response) => {
-            if (response.ok) {
-                response.json()
-                    .then(r => {
-                        r.forEach(vehicle => {
-                            const opt = document.createElement('option');
-                            opt.value = vehicle, opt.text = vehicle;
-                            makesDropDown.add(opt);
-                        });
-                    });
-            } else {
-                console.log('Something went wrong');
-            }
-        })
-        .catch(e => {
-            console.log(new Error(e));
+  // Call and respond to the API
+  fetch(API_VHIC_MODELS)
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((r) => {
+          r.forEach((vehicle) => {
+            const opt = document.createElement('option');
+            (opt.value = vehicle), (opt.text = vehicle);
+            makesDropDown.add(opt);
+          });
         });
-
+      } else {
+        console.log('Something went wrong');
+      }
+    })
+    .catch((e) => {
+      console.log(new Error(e));
+    });
 }
 
 // Fired from the DOM vehicle models dropdown on-change
 function fetchVhicModel(e) {
+  // Clear
+  for (let i = modelDropdown.options.length; i > 0; i--) {
+    modelDropdown.remove(i);
+  }
 
-    // Clear
-    for (let i = modelDropdown.options.length; i > 0; i--) {
-        modelDropdown.remove(i);
-    }
-
-    const model = e.target.value.toLowerCase();
-    if (!model || !model.toString().length) {
-        console.log('Invalid vehicle model');
-        return;
-    }
-    // Call and respond to the API
-    fetch(`${API_VHIC_MODELS}/${model}`)
-        .then((response) => {
-            if (response.ok) {
-                response.json()
-                    .then(r => {
-                        r.forEach(model => {
-                            const opt = document.createElement('option');
-                            opt.value = model, opt.text = model;
-                            modelDropdown.add(opt);
-                        });
-                    });
-            } else {
-                console.log('Something went wrong');
-            }
-        })
-        .catch(e => {
-            console.log(new Error(e));
+  const model = e.target.value.toLowerCase();
+  if (!model || !model.toString().length) {
+    console.log('Invalid vehicle model');
+    return;
+  }
+  // Call and respond to the API
+  fetch(`${API_VHIC_MODELS}/${model}`)
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((r) => {
+          r.forEach((model) => {
+            const opt = document.createElement('option');
+            (opt.value = model), (opt.text = model);
+            modelDropdown.add(opt);
+          });
         });
+      } else {
+        console.log('Something went wrong');
+      }
+    })
+    .catch((e) => {
+      console.log(new Error(e));
+    });
 }
-
 
 // Sets the current form based on the transportation type and hides others
 // Called from the DOM
 function setTransport(ev) {
-    currentForm = document.getElementById(`form-${ev}`);
+  currentForm = document.getElementById(`form-${ev}`);
 
-    planeBlock.hidden = true;
-    vehicleBlock.hidden = true;
-    shippingBlock.hidden = true;
+  planeBlock.hidden = true;
+  vehicleBlock.hidden = true;
+  shippingBlock.hidden = true;
 
-
-    document.getElementById(`${ev}-inputs`).hidden = false;
+  document.getElementById(`${ev}-inputs`).hidden = false;
 }
 
 // Called from the DOM
 // Formats the request and calls the API to generate an estimate
 function getCalc(formType) {
-    let type = formType;
-    const form = currentForm.elements;
-    let data = {};
+  let type = formType;
+  const form = currentForm.elements;
+  let data = {};
 
-    // Create the request object based on the transport type
-    switch (formType) {
-        case 'plane':
-            data = {
-                departure: form.inputDeparture.value,
-                destination: form.inputDestination.value
-            };
-            break;
-        case 'vehicle':
-            data = {
-                make: form.inputMake.value,
-                model: form.inputModel.value,
-                distance: form.inputVehDist.value
-            };
-            break;
-        case 'shipping':
-            data = {
-                weight: form.inputWeight.value,
-                distance: form.inputShipDist.value,
-                method: form.inputShipMethod.value
-            };
-            break;
-    }
+  // Create the request object based on the transport type
+  switch (formType) {
+    case 'plane':
+      data = {
+        departure: form.inputDeparture.value,
+        destination: form.inputDestination.value,
+      };
+      break;
+    case 'vehicle':
+      data = {
+        make: form.inputMake.value,
+        model: form.inputModel.value,
+        distance: form.inputVehDist.value,
+      };
+      break;
+    case 'shipping':
+      data = {
+        weight: form.inputWeight.value,
+        distance: form.inputShipDist.value,
+        method: form.inputShipMethod.value,
+      };
+      break;
+  }
 
-    // Create request headers, body, method, etc.
-    const request = {
-        type,
-        data
-    };
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    const apiInit = {
-        method: 'POST',
-        body: JSON.stringify(request),
-        headers
-    };
+  // Create request headers, body, method, etc.
+  const request = {
+    type,
+    data,
+  };
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  const apiInit = {
+    method: 'POST',
+    body: JSON.stringify(request),
+    headers,
+  };
 
-    // Call and respond to the API
-    fetch(API, apiInit)
-        .then((response) => {
-            if (response.ok) {
-                response.json()
-                    .then(r => updatePage(r));
-            } else {
-                carbonOutput.innerText = 'Well something broke... try again?';
-            }
-        })
-        .catch(() => {
-            console.log(new Error('API failed'));
-            carbonOutput.innerText = 'Well something broke... try again?';
-        })
+  // Call and respond to the API
+  fetch(API, apiInit)
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((r) => updatePage(r));
+      } else {
+        carbonOutput.innerText = 'Well something broke... try again?';
+      }
+    })
+    .catch(() => {
+      console.log(new Error('API failed'));
+      carbonOutput.innerText = 'Well something broke... try again?';
+    });
 }
 
 // Called from JS
 // Binds the estimate values to the DOM
 function updatePage(data) {
-    carbonOutput.innerText = data.data.attributes.carbon_lb + 'lbs.';
-
+  carbonOutput.innerText = data.data.attributes.carbon_lb + 'lbs.';
 }
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
 // Load env vars
 require('dotenv').config();
-const process = require('process')
+const process = require('process');
 // Define basic Express configs
 const express = require('express');
 const app = express();
@@ -13,69 +12,69 @@ const path = require('path');
 // Define some basic endpoints
 const api = '/api/v1';
 const health = `/health`;
-const POST_CALC = '/calculate'
-const GET_VEHICLES = '/vehicles'
-
+const POST_CALC = '/calculate';
+const GET_VEHICLES = '/vehicles';
 
 // Define our target API to proxy to
 const PROXY_API = {
-    key: process.env.SECRET_KEY,
-    url: 'https://www.carboninterface.com/api/v1/estimates',
-    header: new fetch.Headers([
-        ['Authorization', `Bearer ${process.env.SECRET_KEY}`],
-        ['Content-Type', 'application/json']
-    ])
+  key: process.env.SECRET_KEY,
+  url: 'https://www.carboninterface.com/api/v1/estimates',
+  header: new fetch.Headers([
+    ['Authorization', `Bearer ${process.env.SECRET_KEY}`],
+    ['Content-Type', 'application/json'],
+  ]),
 };
 
 const validVehicles = new Map([
-    ['toyota', 1],
-    ['tesla', 2],
-    ['audi', 3],
-    ['ford', 3],
+  ['toyota', 1],
+  ['tesla', 2],
+  ['audi', 3],
+  ['ford', 3],
 ]);
 
 // Create a little logger
 const logger = (req, res, next) => {
-    const timestamp = new Date().toUTCString();
-    const logString = `[LOG] ${timestamp} ${req.originalUrl}`;
-    console.log(logString);
+  const timestamp = new Date().toUTCString();
+  const logString = `[LOG] ${timestamp} ${req.originalUrl}`;
+  console.log(logString);
 
-    next();
-}
+  next();
+};
 app.use(logger);
-
 
 // Define some classes for data because I'm feeling fancy
 class CarbonFlightReq {
-    constructor(type, passengers, legs, unit) {
-        this.type = 'flight';
-        this.passengers = passengers ? passengers : 100;
-        this.legs = [{
-            departure_airport: legs[0],
-            destination_airport: legs[1]
-        }],
-            this.distance_unit = unit ? unit : null;
-    }
+  constructor(type, passengers, legs, unit) {
+    this.type = 'flight';
+    this.passengers = passengers ? passengers : 100;
+    (this.legs = [
+      {
+        departure_airport: legs[0],
+        destination_airport: legs[1],
+      },
+    ]),
+      (this.distance_unit = unit ? unit : null);
+  }
 }
 
 class CarbonVehReq {
-    constructor(type, unit, distance, id) {
-        this.type = type;
-        this.distance_unit = unit;
-        this.distance_value = +distance;
-        this.vehicle_model_id = id;
-    }
+  constructor(type, unit, distance, id) {
+    this.type = type;
+    this.distance_unit = unit;
+    this.distance_value = +distance;
+    this.vehicle_model_id = id;
+  }
 }
 
 class CarbonShipReq {
-    constructor(type, weight, weightU, distance, distanceU, method) {
-        this.type = type;
-        this.weight_value = +weight;
-        this.weight_unit = weightU;
-        this.distance_value = +distance;
-        this.distance_unit = distanceU;
-        this.transport_method = method;
-    }
+  constructor(type, weight, weightU, distance, distanceU, method) {
+    this.type = type;
+    this.weight_value = +weight;
+    this.weight_unit = weightU;
+    this.distance_value = +distance;
+    this.distance_unit = distanceU;
+    this.transport_method = method;
+  }
 }
 
 // Serve static assets and include JSON body parsing middleware
@@ -85,129 +84,151 @@ app.use(express.json());
 
 // Healthcheck endpoints
 app.get(api, (req, res) => {
-    res.send('Hello there!');
+  res.send('Hello there!');
 });
 app.get(`${api}${health}`, (req, res) => {
-    res.send('OK');
+  res.send('OK');
 });
-
 
 // API to fetch vehicle details
 // Returns a collection of strings
 app.get(`${api}${GET_VEHICLES}/:id?`, (req, res) => {
+  if (req.params.id !== null && req.params.id !== undefined) {
+    if (!validVehicles.has(req.params.id.toString().toString())) {
+      console.error('Invalid Vehicle');
+      res.status(400).send('Invalid vehicle make');
+      return;
+    }
 
-    if (req.params.id !== null && req.params.id !== undefined){
-        if(!validVehicles.has(req.params.id.toString().toString())) {
-            console.error('Invalid Vehicle');
-            res.status(400).send('Invalid vehicle make');
-            return;
+    // eslint-disable-next-line no-undef
+    fs.readFile(
+      // eslint-disable-next-line no-undef
+      path.join(__dirname, `data/vehicle-models-${req.params.id}.json`),
+      'utf-8',
+      (error, data) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send(error);
+          return;
         }
 
-        // eslint-disable-next-line no-undef
-        fs.readFile(path.join(__dirname, `data/vehicle-models-${req.params.id}.json`), 'utf-8', (error, data) => {
-            if (error) {
-                console.error(error);
-                res.status(500).send(error);
-                return;
-            }
+        let carData = JSON.parse(data);
+        carData = Array.from(
+          new Set(carData.map((c) => c.data.attributes.name))
+        );
+        res
+          .setHeader('Content-Type', 'application/json')
+          .send(JSON.stringify(carData));
+        return;
+      }
+    );
+  } else {
+    // eslint-disable-next-line no-undef
+    fs.readFile(
+      // eslint-disable-next-line no-undef
+      path.join(__dirname, 'data/vehicle-makes.json'),
+      'utf-8',
+      (error, data) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send(error);
+          return;
+        }
 
-            let carData = JSON.parse(data);
-            carData = Array.from(new Set(
-                carData.map(c => c.data.attributes.name)
-            ));
-            res.setHeader('Content-Type', 'application/json')
-            .send(JSON.stringify(carData));
-            return;
+        let filterData = JSON.parse(data);
+        filterData = filterData
+          .filter((v) =>
+            validVehicles.get(v.data.attributes.name.toLowerCase())
+          )
+          .map((v) => v.data.attributes.name);
 
+        res
+          .setHeader('Content-Type', 'application/json')
+          .send(JSON.stringify(filterData));
+      }
+    );
+  }
+});
 
-        });
-    } else {
-
-        
-        
-        // eslint-disable-next-line no-undef
-        fs.readFile(path.join(__dirname, 'data/vehicle-makes.json'), 'utf-8', (error, data) => {
-            if (error) {
-                console.error(error);
-                res.status(500).send(error);
-                return;
-            }
-            
-            let filterData = JSON.parse(data);
-            filterData = filterData
-            .filter(v => validVehicles.get(v.data.attributes.name.toLowerCase()))
-            .map(v => v.data.attributes.name);
-            
-            res.setHeader('Content-Type', 'application/json')
-            .send(JSON.stringify(filterData));
-        });
-    }
-    });
-
-
-    // API to handle form input
+// API to handle form input
 app.post(`${api}${POST_CALC}`, (request, response) => {
+  if (!request || !request.body || !request.body.data) {
+    response.status(400).send(JSON.stringify(new Error('malformed request')));
+    return;
+  }
 
-    if (!request || !request.body || !request.body.data) {
-        response.status(400).send(JSON.stringify(new Error('malformed request')));
-        return;
-    }
+  if (!PROXY_API || !PROXY_API.key) {
+    response.status(400).send(
+      JSON.stringify({
+        errorCode: 400,
+        errorMessage: 'Mising KEY',
+      })
+    );
+    return;
+  }
 
-    if (!PROXY_API || !PROXY_API.key) {
-        response.status(400).send(JSON.stringify({
-            errorCode: 400,
-            errorMessage: 'Mising KEY'
-        }));
-        return;
-    }
+  let carbonReq = getCarbonReq(request);
 
-    let carbonReq = getCarbonReq(request);
-
-    fetch(PROXY_API.url, {
-        method: 'post',
-        headers: PROXY_API.header,
-        body: JSON.stringify(carbonReq)
+  fetch(PROXY_API.url, {
+    method: 'post',
+    headers: PROXY_API.header,
+    body: JSON.stringify(carbonReq),
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      response.send(json);
     })
-        .then(res => res.json())
-        .then(json => {
-            response.send(json);
-        })
-        .catch(err => {
-            response.status(400).send(err);
-        })
-})
+    .catch((err) => {
+      response.status(400).send(err);
+    });
+});
 
 function getCarbonReq(req) {
-    if (!req || !req.body || !req.body.type) {
-        throw Error('malformed params');
-    }
+  if (!req || !req.body || !req.body.type) {
+    throw Error('malformed params');
+  }
 
-    const data = req.body.data;
-    let carbonReq = {};
-    switch (req.body.type) {
-        case 'plane':
-            carbonReq = new CarbonFlightReq(req.body.type, null, [data.departure, data.destination], 'mi');
-            break;
-        case 'vehicle':
-            carbonReq = new CarbonVehReq(req.body.type, 'mi', data.distance, '7268a9b7-17e8-4c8d-acca-57059252afe9');
-            break;
-        case 'shipping':
-            carbonReq = new CarbonShipReq(req.body.type, data.weight, 'lb', data.distance, 'mi', data.method.toLowerCase());
-            break;
-    }
-    return carbonReq;
+  const data = req.body.data;
+  let carbonReq = {};
+  switch (req.body.type) {
+    case 'plane':
+      carbonReq = new CarbonFlightReq(
+        req.body.type,
+        null,
+        [data.departure, data.destination],
+        'mi'
+      );
+      break;
+    case 'vehicle':
+      carbonReq = new CarbonVehReq(
+        req.body.type,
+        'mi',
+        data.distance,
+        '7268a9b7-17e8-4c8d-acca-57059252afe9'
+      );
+      break;
+    case 'shipping':
+      carbonReq = new CarbonShipReq(
+        req.body.type,
+        data.weight,
+        'lb',
+        data.distance,
+        'mi',
+        data.method.toLowerCase()
+      );
+      break;
+  }
+  return carbonReq;
 }
 
 function validateKey() {
-    if (!PROXY_API.key) {
-        console.error('*** Missing API key*** Is your \'.env\' file created?')
-    }
+  if (!PROXY_API.key) {
+    console.error("*** Missing API key*** Is your '.env' file created?");
+  }
 }
-
-
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Express running on port ${port}`);
-    validateKey();
-})
+  console.log(`Express running on port ${port}`);
+  validateKey();
+});
